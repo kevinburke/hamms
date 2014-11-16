f9d0e7f116215abf23e077fe060da6728c82c627
Kevin Burke <kev@inburke.com>
2014-11-15 22:23:05 -0800

    log incoming requests

--- hamms.py
+++ hamms.py
@@ -15,6 +15,8 @@ logging.basicConfig()
 logger = logging.getLogger("hamms")
 logger.setLevel(logging.INFO)
 
+BASE_PORT = 5500
+
 class HammsServer(object):
     """ Start the hamms server in a thread.
 
@@ -34,8 +36,23 @@ class HammsServer(object):
     def stop(self):
         reactor.stop()
 
+def _log(transport, port, data):
+    try:
+        peer = transport.getPeer()
+        # XXX find user agent
+        topline = data.split('\r\n')[0]
+        return "{ipaddr} {port} \"{topline}\"".format(
+            ipaddr=peer.host, port=port, topline=topline)
+    except Exception:
+        logger.exception("caught exception while formatting log")
+        return "<data received>"
+
 class ListenForeverServer(protocol.Protocol):
-    pass
+
+    PORT = BASE_PORT+1
+
+    def dataReceived(self, data):
+        logger.info(_log(self.transport, self.PORT, data))
 
 
 class ListenForeverFactory(protocol.Factory):
@@ -44,6 +61,11 @@ class ListenForeverFactory(protocol.Factory):
 
 
 class EmptyStringTerminateImmediatelyServer(protocol.Protocol):
+    PORT = BASE_PORT+2
+
+    def dataReceived(self, data):
+        logger.info(_log(self.transport, self.PORT, data))
+
     def connectionMade(self):
         self.transport.write('')
         self.transport.loseConnection()
@@ -55,7 +77,11 @@ class EmptyStringTerminateImmediatelyFactory(protocol.Factory):
 
 
 class EmptyStringTerminateOnReceiveServer(protocol.Protocol):
+
+    PORT = BASE_PORT+3
+
     def dataReceived(self, data):
+        logger.info(_log(self.transport, self.PORT, data))
         self.transport.write('')
         self.transport.loseConnection()
 
@@ -66,6 +92,12 @@ class EmptyStringTerminateOnReceiveFactory(protocol.Factory):
 
 
 class MalformedStringTerminateImmediatelyServer(protocol.Protocol):
+
+    PORT = BASE_PORT + 4
+
+    def dataReceived(self, data):
+        logger.info(_log(self.transport, self.PORT, data))
+
     def connectionMade(self):
         self.transport.write('foo bar')
         self.transport.loseConnection()
@@ -78,11 +110,13 @@ class MalformedStringTerminateImmediatelyFactory(protocol.Factory):
 
 class MalformedStringTerminateOnReceiveServer(protocol.Protocol):
     def dataReceived(self, data):
+        logger.info(_log(self.transport, self.PORT, data))
         self.transport.write('foo bar')
         self.transport.loseConnection()
 
 
 class MalformedStringTerminateOnReceiveFactory(protocol.Factory):
+
     def buildProtocol(self, addr):
         return MalformedStringTerminateOnReceiveServer()
 
@@ -96,6 +130,7 @@ class FiveSecondByteResponseServer(protocol.Protocol):
         self.transport.write(byte)
 
     def dataReceived(self, data):
+        logger.info(_log(self.transport, self.PORT, data))
         timer = 5
         for byte in empty_response:
             reactor.callLater(timer, self._send_byte, byte)
@@ -114,6 +149,7 @@ class ThirtySecondByteResponseServer(protocol.Protocol):
         self.transport.write(byte)
 
     def dataReceived(self, data):
+        logger.info(_log(self.transport, self.PORT, data))
         timer = 30
         for byte in empty_response:
             reactor.callLater(timer, self._send_byte, byte)
@@ -127,6 +163,8 @@ class ThirtySecondByteResponseFactory(protocol.Factory):
 
 
 class SendDataPastContentLengthServer(protocol.Protocol):
+    def dataReceived(self, data):
+        logger.info(_log(self.transport, self.PORT, data))
     def connectionMade(self):
         self.transport.write('HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n'
                              'Content-Length: 3\r\n\r\n' + 'a'*1024*1024)
@@ -143,6 +181,7 @@ def success_response(content_type, response):
 
 class DropRandomRequestsServer(protocol.Protocol):
     def dataReceived(self, data):
+        logger.info(_log(self.transport, self.PORT, data))
         body = data.split('\r\n')
         method, url, http_vsn = body[0].split(' ')
         o = urlparse.urlparse(url)
@@ -199,12 +238,14 @@ large_header_resource = WSGIResource(reactor, reactor.getThreadPool(),
                                      large_header_app)
 large_header_site = Site(large_header_resource)
 
-BASE_PORT = 5500
 
-reactor.listenTCP(BASE_PORT+1, ListenForeverFactory())
-reactor.listenTCP(BASE_PORT+2, EmptyStringTerminateImmediatelyFactory())
-reactor.listenTCP(BASE_PORT+3, EmptyStringTerminateOnReceiveFactory())
-reactor.listenTCP(BASE_PORT+4, MalformedStringTerminateImmediatelyFactory())
+reactor.listenTCP(ListenForeverServer.PORT, ListenForeverFactory())
+reactor.listenTCP(EmptyStringTerminateImmediatelyServer.PORT,
+                  EmptyStringTerminateImmediatelyFactory())
+reactor.listenTCP(EmptyStringTerminateOnReceiveServer.PORT,
+                  EmptyStringTerminateOnReceiveFactory())
+reactor.listenTCP(MalformedStringTerminateImmediatelyServer.PORT,
+                  MalformedStringTerminateImmediatelyFactory())
 reactor.listenTCP(BASE_PORT+5, MalformedStringTerminateOnReceiveFactory())
 reactor.listenTCP(BASE_PORT+6, FiveSecondByteResponseFactory())
 reactor.listenTCP(BASE_PORT+7, ThirtySecondByteResponseFactory())
