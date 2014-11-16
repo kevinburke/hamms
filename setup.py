ab9c53456c110ae6d6fb6f2c053a275e0c883ca9
Kevin Burke <kev@inburke.com>
2014-11-15 22:34:31 -0800

    specify requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from distutils.core import setup
 
-with open('requirements.txt') as f:
-    reqs = f.read().strip().split('\n')
-
 setup(
     name='hamms',
     packages=['hamms'],
-    version='0.3',
+    version='0.4',
     description='Malformed servers to test your HTTP client',
     author='Kevin Burke',
     author_email='kev@inburke.com',
     url='https://github.com/kevinburke/hamms',
     keywords=['testing', 'server', 'http',],
-    requires=reqs,
+    # XXX, pin these down
+    requires=['flask', 'httpbin', 'twisted'],
 )
