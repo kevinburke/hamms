037eca6396178cf36fb305315144e5d711d1c163
Kevin Burke <kev@inburke.com>
2014-11-15 22:25:27 -0800

    prepare code for pypi release

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='hamms',
+    packages=['hamms'],
+    version='0.1',
+    description='Malformed servers to test your HTTP client',
+    author='Kevin Burke',
+    author_email='kev@inburke.com',
+    url='https://github.com/kevinburke/hamms',
+    keywords=['testing', 'server', 'http',]
+)
