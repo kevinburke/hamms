fcf46548cf14e45be64178afd4870df5057deaf0
Kevin Burke <kev@inburke.com>
2014-11-15 22:32:41 -0800

    update requirements list

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open('requirements.txt') as f:
 setup(
     name='hamms',
     packages=['hamms'],
-    version='0.2',
+    version='0.3',
     description='Malformed servers to test your HTTP client',
     author='Kevin Burke',
     author_email='kev@inburke.com',
     url='https://github.com/kevinburke/hamms',
     keywords=['testing', 'server', 'http',],
-    install_requires=reqs,
+    requires=reqs,
 )
