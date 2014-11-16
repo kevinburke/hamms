f9f09a52cb79ed671320fec8c123faf3ed437417
Kevin Burke <kev@inburke.com>
2014-11-15 22:31:40 -0800

    add correct requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+    reqs = f.read().strip().split('\n')
+
 setup(
     name='hamms',
     packages=['hamms'],
-    version='0.1',
+    version='0.2',
     description='Malformed servers to test your HTTP client',
     author='Kevin Burke',
     author_email='kev@inburke.com',
     url='https://github.com/kevinburke/hamms',
-    keywords=['testing', 'server', 'http',]
+    keywords=['testing', 'server', 'http',],
+    install_requires=reqs,
 )
