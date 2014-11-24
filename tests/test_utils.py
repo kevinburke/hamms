6a28dd91d1a9167e60f262365afcf7d717222e0e
Kevin Burke <kev@inburke.com>
2014-11-23 19:40:51 -0800

    update test to match interface

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 from nose.tools import assert_equal
 
-from hamms import get_user_agent
+from hamms import get_header
 
 req = "\r\n".join(["GET / HTTP/1.0",
                    "User-Agent: my-user-agent",
@@ -10,5 +10,5 @@ noreq = "\r\n".join(["GET / HTTP/1.0",
                      "Accept: */*", "\r\n"])
 
 def test_user_agent():
-    assert_equal("my-user-agent", get_user_agent(req))
-    assert_equal("", get_user_agent(noreq))
+    assert_equal("my-user-agent", get_header("user-agent", req))
+    assert_equal("", get_header("user-agent", noreq))
